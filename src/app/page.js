821badbb238ca07639758f5f3e9b906da5011164
Vapoor <kylian.gagnant@orange.fr>
2025-01-23 11:42:50 +0100

    Fix small errors
    Tailwind finally working, deleted Darkmode for Hydratation React error

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,10 @@ import {DarkMode} from './utils/Darkmode';
 export default function Home(){
   return (
     <React.StrictMode>
-    <html>
-      <body>
-      
     <section id="welcome">
       <h1>Welcome :)</h1>
     </section>
     <SetInputReader/>
-      </body>
-    </html>
     </React.StrictMode>
   )
 }
--- src/app/utils/FileReader.js
+++ src/app/utils/FileReader.js
@@ -1,8 +1,8 @@
 
 export function SetInputReader(){
     return (
-        <div className="grid bg-sky-300 content-center">
-            <h3 className="bg-sky-300">In file reader :D</h3>
+        <div className="grid content-center">
+            <h3 className="">In file reader :D</h3>
         </div>
     )
 }
