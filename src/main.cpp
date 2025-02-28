aaabd9c1f1e723e3e5252b402360ec8d7ec73415
Théophile "Inkurey" R. <inkurey22.tr@proton.me>
2025-02-28 12:38:08 +0100

    FEAT: now takes a path as a directory to read the musics from

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "songLoader.hpp"
 
 int main(int argc, char* argv[]){
-    songLoader test = songLoader("./test");
+    if (argc != 2){
+        std::cout << "Usage: " << argv[0] << " <path to music folder>" << std::endl;
+        return 1;
+    }
+    songLoader test = songLoader(argv[1]);
     // test.debugSongs();
     test.listenSong();
     return 0;
